b60b3dc4cc8e3e7079f61909da9c88af7f6900fd
rocketship92 <tsmyth92@gmail.com>
2017-01-30 09:17:22 +0000

    Added scripts directory

---
+++ scripts/multigrep.sh
@@ -0,0 +1,12 @@
+if [ -z $1 ]; then                                              # check user has passed in search terms
+        echo "pass in search terms"
+else
+
+    data="ps -ef | grep $USER"                                  # search processed for current user
+    for term in $@; do
+        data="$data "'| grep '"$term"                           # grep for each term separately
+    done
+
+    eval $data                                                  # evaluate grep statement
+
+fi
