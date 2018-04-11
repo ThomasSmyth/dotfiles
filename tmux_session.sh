a05a8ba71c7482055ad6a19b3dcce4c22f1bde5c
Thomas Smyth <13893844+vibronicshark55@users.noreply.github.com>
2018-04-12 00:55:22 +0100

    Add example automated tmux script

---
+++ tmux_session.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# tmux start-server
+
+# start new session and split resulting window into 2 panes
+tmux new-session -d -s dev -n main \; \
+  send-keys 'cd ~' C-m \; \
+  split-window -h \; \
+  send-keys 'cd ~' C-m \;
+
+# split window into 2 panes horizontally
+tmux new-window -t dev:1 -n filex \; \
+  send-keys 'cd ~' C-m \; \
+  split-window -h \; \
+  send-keys 'cd ~' C-m \;
+
+# split window into 3 panes similar sized panes vertically
+tmux new-window -t dev:2 \; \
+  send-keys 'cd ~' C-m \; \
+  split-window -v -p 66 \; \
+  split-window -v \;
+
+# attach to session on window 0
+tmux select-window -t dev:0
+tmux attach-session -t dev
+
+# tmux kill-server
