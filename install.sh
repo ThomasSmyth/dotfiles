aa75ad10a6f0ce24b72cc626f0ffcd745b70ee2f
Thomas Smyth <tsmyth92@gmail.com>
2018-04-17 23:22:32 +0100

    #2 Merge archive functionality to install script

--- install.sh
+++ install.sh
@@ -1,14 +1,49 @@
 #!/usr/bin/env bash
 
-## initialise settings
+############
+# settings #
+############
 
-rootc=$PWD/$(dirname "${BASH_SOURCE}")                                  # return custom settings directory
-dfiles=$rootc/dotfiles
+# archive settings
+archive=1                                                               # archive if set to 1
+archdir=~/.archive                                                      # archive directory
+dotdir=$HOME                                                            # install location for dotfiles
+
+# other
 stgs=""
 
-# install location
-archdir=~/.archive
-dotdir=$HOME
+# dotfile locations
+rootc=$PWD/$(dirname "${BASH_SOURCE}")                                  # full path dotfiles repo
+dfiles=$rootc/dotfiles                                                  # full path to dotfiles sub directory
+
+#######################
+# archiving functions #
+#######################
+recurseFiles () {                                                       # return files form all sub directories
+  dir=$(echo "$1" | sed 's|\/*$||g')                                    # trim trailing forward slash
+  find $dir -type f | sed "s|^$dir\/||g"                                # return file paths of dotfiles
+ }
+
+archiveFile() {                                                         # archive file if it exists
+  oldFile=$dotdir/$1
+  if [ -f $oldFile ]; then
+    echo "archiving $oldFile"
+    mkdir -p $archdir/$(dirname $1)
+    cp $oldFile $archdir/$1
+  fi
+ }
+
+archiveAllFiles() {                                                     # archive all dotfiles
+  newFiles=($(recurseFiles $dfiles))                                    # store files to install as array
+
+  for file in ${newFiles[@]}; do                                        # iterate over dotfiles
+    archiveFile $file                                                   # archive dotfile to $archdir
+  done
+ }
+
+################
+# installation #
+################
 
 for stg in $@; do
   if [ "all" = $stg ]; then
@@ -36,6 +71,13 @@ for stg in $stgs; do
     ;;
 
     dotfiles )
+      if [ 1 -ne $archive ]; then                                       # archive dotfiles if enabled
+        echo "archiving not enabled"
+      else
+        echo "archiving enabled"
+        archiveAllFiles
+      fi
+
       echo "adding dotfiles"                                            # add dotfiles
       cp -rsf $dfiles/. $HOME                                           # symlink dotfiles to homedir
     ;;
@@ -134,6 +176,10 @@ for stg in $stgs; do
   esac
 
 done
+
+##############
+# post steps #
+##############
 
 stgs=""
 
